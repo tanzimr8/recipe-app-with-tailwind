b1b1285c2d78979bb8a76cc1a5a2593eec04c68c
Tanzim <tanzim008@gmail.com>
2024-07-14 01:00:16 -0230

    nav component created

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// Pages
+    //**************
+    // Home
+    // Favorites
+    // Details
+    // *************
+    // Components
+    // *************
+    // Home
+    // Nav
+    // Recipes
+    // Recipe
+    // Contexts
+    // Favorites
+    // Favorite
+    // Details
+    // Routes
+import './index.css';
+import Nav from './components/shared/Nav';
+
+function App() {
+  return (
+    <Nav/>
+  );
+}
+
+export default App;
