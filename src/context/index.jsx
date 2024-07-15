b931503d688a714386ff2085f66afddd56865ba6
Tanzim <tanzim008@gmail.com>
2024-07-14 23:51:19 -0230

    mported card

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,14 +1,37 @@
 import React, { createContext, useState } from 'react'
-
+//CONTEXT CREATION
 export const GlobalContext = createContext();
 const GlobalState = ({children}) => {
+  //DECLARING ALL STATES
   const [searchInput, setSearchInput] = useState('');
-  const  handleSearch = (event)=>{
+  const [recipeLists,setRecipeLists] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const [error,setError] = useState(false);
+  
+  //HANDLE ON SUBMIT ON THE SEARCH FORM
+  const handleSearch = async (event)=>{
     event.preventDefault();
+    try{
+      const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchInput}`);
+      const data = await response.json();
+      if(data?.data?.recipes){
+        setRecipeLists(data?.data?.recipes);
+        setSearchInput('');
+        setError(false);
+        setLoading(false);
+      }
+    }
+    catch(e){
+      setError(e);
+      setLoading(false);
+      setSearchInput('');
+    }
   }
+  
   return (
+    //PROVIDE THE CONTEXT
     <GlobalContext.Provider
-    value={{searchInput, setSearchInput,handleSearch}}
+    value={{searchInput, setSearchInput,handleSearch,recipeLists,setRecipeLists}}
     >{children}</GlobalContext.Provider>
   )
 }
